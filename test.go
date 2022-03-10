815a62f9dd794247188a05ecab27f8d4dbbe4af2
Danihoosh <62504448+Danihoosh@users.noreply.github.com>
2022-03-10 14:59:11 +0330

    Update test.go

--- test.go
+++ test.go
@@ -1,7 +1,13 @@
 package main
 
-import ("fmt")
+import ("fmt"
+       "math")
 
 func main() {
-fmt.Print("Hello-Testing-CI")
+  sum(2,3)
+}
+
+func sum(num1 int,num2 int) power int{
+  power :=num1 * num2
+  return power
 }
